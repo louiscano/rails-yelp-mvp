78df9556e7edfdde103467cc98c20b854bdf644c
Louis Cano <louis.cano@hotmail.fr>
2024-07-03 16:53:21 +0200

    exercice 2 Yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurant/index'
+  get 'restaurant/new'
+  get 'restaurant/create'
+  get 'restaurant/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +13,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants, except: [:edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,45 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Restaurant.destroy_all
+
+tour_d_argent = Restaurant.new(
+  name: "La Tour d'Argent",
+  address: '15 Quai de la Tournelle, 75005 Paris',
+  phone_number: '01 43 54 23 31',
+  category: 'french'
+)
+tour_d_argent.save!
+
+chez_gladines = Restaurant.new(
+  name: 'Chez Gladines',
+  address: '11 Bis Rue des Halles, 75001, Paris',
+  phone_number: '01 42 21 07 00',
+  category: 'french'
+)
+chez_gladines.save!
+
+sushi_shop = Restaurant.new(
+  name: 'Sushi Shop',
+  address: '24 Rue du Dr Charles Nancel Penard, 33000 Bordeaux',
+  phone_number: '08 26 82 66 28',
+  category: 'japanese'
+)
+sushi_shop.save!
+
+grain_de_riz = Restaurant.new(
+  name: 'Grain De Riz',
+  address: '109 Boulevard De La République, 33510 Andernos',
+  phone_number: '05 56 26 25 02',
+  category: 'chinese'
+)
+grain_de_riz.save!
+
+dodo_pizza = Restaurant.new(
+  name: 'Dodo Pizza',
+  address: '8 Bis Av. du Général de Gaulle, 33510 Andernos',
+  phone_number: '05 56 26 00 42',
+  category: 'italian'
+)
+dodo_pizza.save!
